16e1f854a00b65206cced5654cca68eaf5386155
syipaaln <syipalatipa@gmail.com>
2024-07-11 10:43:44 +0700

    landing page(belum login)

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,16 +6,57 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Data Products - Darma</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css" rel="stylesheet">
 </head>
-<body style="background: lightgray">
+<body class="bg-light">
+    <nav class="navbar bg-white">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('images/logo.png') }}" alt="TB Darma" width="150" height="24">
+            </a>
+            <div class="d-flex">
+                @guest
+                    @if (Route::has('register'))
+                        <a class="btn btn-outline-primary me-2" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    @endif
+                    
+                    @if (Route::has('login'))
+                        <a class="btn btn-primary" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    @endif
+                @else
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
 
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                @endguest
+            </div>
+        </div>
+    </nav>
     <div class="container mt-5">
-        <div class="row">
+        <div class="card border-0 shadow-sm rounded text-center py-5 mb-2">
+            <div class="card-body">
+                ini banner
+            </div>
+        </div>
+        <div class="d-flex justify-content-between align-items-center my-5">
+            <h5>PRODUK</h5>
+            <form class="search-form" style="max-width: 200px;">
+                <input class="form-control" type="search" placeholder="Cari Produk" aria-label="Search">
+            </form>
+        </div>
+        {{-- <div class="row">
             <div class="col-md-12">
-                <div>
-                    <h3 class="text-center my-4">Darma</h3>
-                    <hr>
-                </div>
                 <div class="card border-0 shadow-sm rounded">
                     <div class="card-body">
                         <a href="{{ route('products.create') }}" class="btn btn-md btn-success mb-3">ADD PRODUCT</a>
@@ -59,6 +100,30 @@
                     </div>
                 </div>
             </div>
+        </div> --}}
+        <div class="row mt-3">
+            @forelse ($products as $product)
+                <div class="col-md-4 col-lg-3 col-xl-2 mb-4">
+                    <div class="card product-card h-100">
+                        <img src="{{ asset('/storage/products/'.$product->image) }}" class="card-img-top" alt="Product Image">
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title fs-6 mb-4">{{ $product->title }}</h5>
+                            <div class="mt-auto">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <p class="card-text fw-bold mb-0">{{ "Rp " . number_format($product->price,2,',','.') }}</p>
+                                    <a href="{{ route('login') }}" class="btn btn-primary btn-sm d-flex align-items-center">
+                                        <i class="bi bi-cart-plus"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <div class="alert alert-danger">
+                    Data Products belum Tersedia.
+                </div>
+            @endforelse
         </div>
     </div>
 
