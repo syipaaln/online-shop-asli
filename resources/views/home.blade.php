1441120ff51f18dd37533862ae7ca8733664dac3
tinasaf <tinasafitri212@gmail.com>
2024-08-01 12:47:17 +0700

    Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+        .bg {
+            background-color: #F6F6F6;
+        }
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus, consequatur. Sint consequatur earum quibusdam, delectus eum quaerat alias illo nostrum repudiandae quia ipsum a enim iure. Explicabo, doloremque. Exercitationem, quia?
-                    {{ __('You are logged in!') }}
+        .card-custom {
+            padding: 1rem;
+            margin: 0.5rem;
+            width: calc(100% - 1rem);
+            cursor: pointer;
+            /* border: 1px solid rgba(0, 0, 0, 0.1); //kalo pake inih ada garisan */
+            border: none; 
+            box-shadow: 0 0 5px rgba(90, 90, 90, 0.1); tambah ini
+        }
+    </style>
+</head>
+<body class="bg-light-blue">
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-md-3">
+                <div class="card text-center card-custom">
+                    <div class="card-body">
+                        <h2 class="card-title text-primary">120</h2>
+                        <p class="card-text">Penjualan Hari Ini</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card text-center card-custom">
+                    <div class="card-body">
+                        <h2 class="card-title text-primary">120</h2>
+                        <p class="card-text">User Daftar Hari Ini</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card text-center card-custom">
+                    <div class="card-body">
+                        <h2 class="card-title text-primary">120</h2>
+                        <p class="card-text">Penjualan Bulan Ini</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card text-center card-custom">
+                    <div class="card-body">
+                        <h2 class="card-title text-primary">120</h2>
+                        <p class="card-text">User Daftar Bulan Ini</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <div class="col-md-6">
+                <div class="card text-center card-custom">
+                    <div class="card-body">
+                        <p class="card-text">Total Pendapatan Hari Ini</p>
+                        <h2 class="card-title text-primary">Rp24.000.000,00-</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card text-center card-custom">
+                    <div class="card-body">
+                        <p class="card-text">Total Pendapatan Bulan Ini</p>
+                        <h2 class="card-title text-primary">Rp24.000.000,00-</h2>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</body>
+</html>
+
 @endsection
+
