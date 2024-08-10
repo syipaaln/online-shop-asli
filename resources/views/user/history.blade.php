00b0a2f30dbc2dd141d61ff9b0e1a724326638c2
tinasaf <tinasafitri212@gmail.com>
2024-08-10 14:17:29 +0700

    UpdateInvo

--- resources/views/user/history.blade.php
+++ resources/views/user/history.blade.php
@@ -1,6 +1,32 @@
 @extends('layouts.user')
 
 @section('content')
+
+<style>
+    .button-container {
+            display: flex;
+            justify-content: center;
+        }
+    button {
+        background-color: #5dc761;
+        color: white; 
+        border: none; 
+        padding: 10px 20px; 
+        text-align: center; 
+        display: inline-block;
+        font-size: 12px;
+        border-radius: 5px; 
+        cursor: pointer; 
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); 
+    }
+    button:hover {
+        background-color: #62be66; 
+    }
+
+    button:focus {
+        outline: none;
+    }
+</style>
     <div class="container">
         <h1>History Pembelian</h1>
 
@@ -29,10 +55,11 @@
                             </td>
                             <td>{{ $group['total_price'] }}</td>
                             <td>
-                                <form action="{{ route('generateInvoice', ['date' => $group['date']]) }}" method="GET">
-                                    @csrf
-                                    <button type="submit">Cetak Invoice</button>
-                                </form>
+                                <div class="button-container">
+                                    <button onclick="window.location.href='{{ route('generateInvoice', ['date' => $group['date']]) }}'">
+                                        Cetak Invoice
+                                    </button>
+                                </div>
                             </td>
                         </tr>
                     @endforeach
--- resources/views/user/invoice.blade.php
+++ resources/views/user/invoice.blade.php
@@ -2,30 +2,95 @@
 <html>
 <head>
     <title>Invoice</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        .logo {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .logo img {
+            max-height: 80px;
+            width: auto;
+        }
+        h1 {
+            text-align: center;
+            font-size: 24px;
+            margin-bottom: 20px;
+        }
+        p {
+            font-size: 14px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+        th {
+            padding: 8px;
+            text-align: center;
+            border: 1px solid rgb(91, 91, 91);
+        }
+        td {
+            padding: 8px;
+            text-align: left;
+            border: none;
+        }
+        .right-align {
+            text-align: right;
+        }
+        .center-align {
+            text-align: center;
+        }
+        .total-price {
+            text-align: right;
+            margin-top: 20px;
+            font-weight: bold;
+        }
+        .text-center {
+            text-align: center;
+            margin-top: 20px; 
+            font-size: 18px;
+        }
+        .date{
+            text-align: right;
+        }
+        .dashed-divider {
+            border-top: dashed black; 
+            width: fit-content;
+            margin: 10px auto 20px auto;
+
+        }
+    </style>
 </head>
 <body>
-    <h1>Invoice</h1>
-    <p>Date: {{ $date }}</p>
-
+    <div class="logo">
+        <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/logo.png'))) }}" alt="Logo">
+    </div>
+    <p class="date">Date: {{ $date }}</p>
     <table>
         <thead>
             <tr>
+                <th>No</th>
                 <th>Product ID</th>
-                {{-- <th>Quantity</th> --}}
-                <th>Price</th>
+                {{-- <th class="center-align">Quantity</th> --}}
+                <th class="right-align">Total Price</th>
             </tr>
         </thead>
         <tbody>
             @foreach($histories as $history)
                 <tr>
+                    <td class="center-align">{{$history->id}}</td>
                     <td>{{ $history->product_id }}</td>
-                    {{-- <td>{{ $history->checkout->quantity }}</td> --}}
-                    <td>{{ $history->price }}</td>
+                    {{-- <td class="center-align">{{ $history->quantity }}</td> --}}
+                    <td class="right-align">{{ number_format($history->price, 0, ',', '.') }}</td>
                 </tr>
             @endforeach
         </tbody>
     </table>
-
-    <p>Total Price: {{ $totalPrice }}</p>
+    <div class="dashed-divider"></div>
+    <p class="total-price">Total Price: {{ number_format($totalPrice, 0, ',', '.') }}</p>
+    <h4 class="text-center"> THANK YOU FOR YOUR VISIT </h4>
 </body>
 </html>
